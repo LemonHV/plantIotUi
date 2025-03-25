30d6e7e57a04a14bc8d72a08db279c12f3a4028e
LemonHV <matruongvu1@gmail.com>
2025-03-25 14:40:01 +0700

    add api base url

--- lib/api.ts
+++ lib/api.ts
@@ -6,8 +6,7 @@ export interface FeedData {
   value: string;
 }
 
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:5000";
+const API_BASE_URL = process.env.PORT ||  "http://127.0.0.1:10000";
 const API_FEED_URL = process.env.NEXT_PUBLIC_API_DIRECT;
 const PUBLIC_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
